78e7c0a2534c96cee7eb9162ce9c6a5643ae6eb2
Robert <baxterrp@gmail.com>
2016-12-02 09:04:12 -0500

    working test for acceptCoins returns coin value

--- src/Machine.java
+++ src/Machine.java
@@ -1,4 +1,15 @@
 
 public class Machine {
+	
+	private double coinTotal = 0;
+	private String display = "Insert Coin";
+	
+	public double acceptCoins(String coinName){
+		switch(coinName){
+				case "nickel" : return .05;
+		}
+		
+		return 0;
+	}
 
 }
--- src/VendingMachineTest.java
+++ src/VendingMachineTest.java
@@ -1,9 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class VendingMachineTest {
+	
+	Machine machine;
 
+	@Before
+	public void setUp(){
+		machine = new Machine();
+	}
 	////Accept Coins Tests
 	
 	//returns coin value
+	@Test
+	public void testAcceptCoinsForCoinValue(){
+		assertEquals(.05, machine.acceptCoins("nickel"), 0);
+	}
 	
 	//increments customers total coin value
 	
