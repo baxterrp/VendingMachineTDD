18ae5ff506beaaa108a61b6bda6f932760f0468a
Robert <baxterrp@gmail.com>
2016-12-02 08:55:54 -0500

    planned tests for acceptCoins method

---
+++ src/VendingMachineTest.java
@@ -0,0 +1,13 @@
+
+public class VendingMachineTest {
+
+	////Accept Coins Tests
+	
+	//returns coin value
+	
+	//increments customers total coin value
+	
+	//display is updated when coin is inserted
+
+	////Select Product Tests
+}
